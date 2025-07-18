f1060d8f12b537a082aa3f6eaf26071ec6fd8402
Jasmin Sultana Shimu <jasmin851t626@gmail.com>
2025-07-19 01:44:28 +0600

    portfolio_2

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+
+import Header from "./components/header";
+
+import Hero from "./components/Hero";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
+
+export default function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1500,
+      once: true,
+    });
+  }, []);
+
+  return (
+    <main>
+      {/* Gradient Image */}
+      <img
+        className="absolute top-0 right-0 opacity-60 -z-10"
+        src="/gradient.png"
+        alt="Background violet gradient decoration"
+      />
+
+      {/* Blur Effect */}
+      <div
+        className="h-0 w-[40rem] absolute top-[20%] right-[-5%] shadow-[0_0_900px_20px_#8F00FF] -rotate-[30deg] -z-10"
+      ></div>
+
+      <Header />
+      <Hero />
+    </main>
+  );
+}
---
+++ src/components/Hero.jsx
@@ -0,0 +1,54 @@
+import 'boxicons/css/boxicons.min.css';
+import Spline from '@splinetool/react-spline';
+
+const Hero = () => {
+    return (
+        <main className="flex lg:mt-20 flex-col lg:flex-row items-center justify-between min-h-[calc(90vh - 6rem)]">
+
+            <div data-aos="fade-right"
+                data-aos-offset="300"
+                data-aos-easing="ease-in-sine" className="max-w-xl ml-[5%] z-10 mt-[90%] md:mt-[60%] lg:mt-0">
+
+                {/* Tag box with gradient border */}
+                <div className="relative w-[95%] sm:w-48 h-10 bg-gradient-to-r from-[#8F00FF] to-[#e99b63] shadow-[0_0_15px_rgba(255,255,255,0.4)] rounded-full">
+                    <div className="absolute inset-[3px] bg-black rounded-full flex items-center justify-center gap-1">
+                        <i className="bx bx-star"></i>
+                        Creative Portfolio
+                    </div>
+                </div>
+
+                {/* Main Heading with 3D Text Effect for "Violet" */}
+                <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold tracking-wider my-8 text-[#8F00FF] violet-3d-text">
+                    Violet
+                </h1>
+
+                {/* Description */}
+                <p className="text-base sm:text-lg tracking-wider text-gray-400 max-w-[25rem] lg:max-w-[30rem]">
+                    Crafting innovative and inspiring projects with creativity and passion.
+                </p>
+
+                {/* Buttons */}
+                <div className='flex gap-4 mt-12'>
+                    <a className='border border-[#2a2a2a] py-2 sm:py-3 px-4 sm:px-5 rounded-full sm:text-lg text-sm font-semibold tracking-wider transition-all duration-300 hover:bg-[#8F00FF] hover:text-white' href="#projects">
+                        View Projects
+                        <i className='bx bx-right-arrow-alt ml-2'></i>
+                    </a>
+
+                    <a className='border border-[#2a2a2a] py-2 sm:py-3 px-4 rounded-full sm:text-lg text-sm font-semibold tracking-wider transition-all duration-300 bg-[#6B00CC] text-white hover:bg-[#ffffff] hover:text-[#8F00FF]' href="#contact">
+                        Contact Me
+                        <i className='bx bx-envelope ml-2'></i>
+                    </a>
+                </div>
+            </div>
+
+            {/* 3D Robot */}
+            <Spline data-aos="fade-zoom-in"
+                data-aos-easing="ease-in-back"
+                data-aos-delay="300"
+                data-aos-offset="0" data-aos-duration="3000" className='absolute lg:top-0 top-[-20%] bottom-0 lg:left-[25%] sm:left-[-2%] h-full' scene="https://prod.spline.design/nPVDoD9fGkNw3Q0X/scene.splinecode" />
+
+        </main>
+    );
+};
+
+export default Hero;
